f81460d547817ef79f93e6eafcab13eb11ad3c49
Gardennias <tsmzhi@outlook.com>
2023-02-08 16:34:36 +0800

    deploy

---
+++ assets/index.7f2b271f.js
@@ -0,0 +1,1 @@
+import{h as s,u,i as o}from"./index.a84b3b6d.js";const p={name:"Redirect",setup(){const e=o(),t=u(),{query:r,params:a}=e;return t.replace({path:"/"+a.path,query:r}),()=>s("template")}};export{p as default};
