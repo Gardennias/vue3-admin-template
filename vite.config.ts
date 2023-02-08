9ff251563fdc95eddf805697557f509a9d4c3a1a
Gardennias <tsmzhi@outlook.com>
2023-02-08 17:56:02 +0800

    feat: update

--- vite.config.ts
+++ vite.config.ts
@@ -7,10 +7,10 @@ import Components from "unplugin-vue-components/vite";
 import {ElementPlusResolver} from "unplugin-vue-components/resolvers"
 import ElementPlus from "unplugin-element-plus/vite"
 import DefineOptions from "unplugin-vue-define-options/vite";
-// import {createStyleImportPlugin, ElementPlusResolve} from "vite-plugin-style-import"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/vue3-admin-template/',
   server: { // 后台接口服务器配置
     proxy: {
       "/dev-api": {
